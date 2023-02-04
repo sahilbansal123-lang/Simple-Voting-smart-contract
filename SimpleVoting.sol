70f4d1f77d43106bb6ebe0f029795fff664c8362
SAHIL BANSAL <72665907+sahilbansal123-lang@users.noreply.github.com>
2023-02-04 12:00:52 +0530

    Update SimpleVoting.sol

--- SimpleVoting.sol
+++ SimpleVoting.sol
@@ -4,13 +4,19 @@ pragma solidity ^0.8.0;
 
 contract Voting {
     uint public totalVotes;
-    mapping(uint => uint) public votes;
+    mapping(uint => uint) public votes;       //votes mapping tracks the number of votes received for each proposal.
+
+//he vote function accepts a proposal as an argument and 
+//increments the vote count for that proposal in the votes mapping
 
     function vote(uint proposal) public {
         require(proposal >= 0 && proposal <= 1, "Invalid proposal.");
         votes[proposal]++;
         totalVotes++;
     }
+
+// winningProposal function returns the winning proposal by iterating through the votes mapping 
+//and finding the proposal with the highest number of votes.
 
     function winningProposal() public view returns (uint) {
         uint winningVoteCount = 0;
